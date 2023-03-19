1c93cbea78cef0484ece79c56ab67ead960cf805
victor-oliveira-dev98 <107275644+victor-oliveira-dev98@users.noreply.github.com>
2023-03-19 22:01:40 +0000

    Add files via upload

---
+++ firstapi/src/mocks/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'matheus',        
+            
+    },
+    {
+        id: 2,
+        name: 'Jose',        
+            
+    },
+
+    {
+        id: 3,
+        name: 'Rose',        
+            
+    }
+
+
+]
